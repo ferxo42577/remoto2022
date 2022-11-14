2c2b112c0482caa6019f2afec4ec0abff74dd28a
Fertunet <ferxo.ypr@gmail.com>
2022-11-14 18:30:32 -0400

    primer commit

---
+++ conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+function conectar(){
+
+    
+    
+    $DB_HOST = $_ENV['DB_HOST'];
+    $DB_USER = $_ENV['DB_USER']; 
+    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
+    $DB_NAME = $_ENV['DB_NAME'];
+    $DB_PORT = $_ENV['DB_PORT'];
+
+    //$user = "root"; 
+    //$pass = "";
+    //$server = "localhost";
+    //$db = "saboresdeliz";
+
+
+    $enlace = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME,$DB_PORT);    
+    
+    if (!$enlace){
+        die ("No pudo conectarse a la base de datos");
+    }
+
+    echo "<center>Conexion exitosa ok</center><br/>";
+
+    return $enlace;
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php   
+include("conexion.php");
+$con=conectar();
+
+echo "<center>";
+echo "<h1>Bienvenidos al rest Sabores de liz 2022 -- </h1>";
+echo "</center>";
+?>
