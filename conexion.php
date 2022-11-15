3592f4edac7df8f0542f728e9a7e208f879e541a
Fertunet <ferxo.ypr@gmail.com>
2022-11-15 18:45:29 -0400

    add: puerto

--- conexion.php
+++ conexion.php
@@ -8,15 +8,20 @@ function conectar(){
     $DB_USER = $_ENV['DB_USER']; 
     $DB_PASSWORD = $_ENV['DB_PASSWORD'];
     $DB_NAME = $_ENV['DB_NAME'];
-    $DB_PORT = $_ENV['DB_PORT'];
+    $DB_PORT = "7036";
+    //$DB_PORT = $_ENV['DB_PORT']; 
+    // puerto server: 7036
 
-    //$user = "root"; 
-    //$pass = "";
-    //$server = "localhost";
-    //$db = "saboresdeliz";
-
+    /*
+    $user = "root"; 
+    $pass = "";
+    $server = "localhost";
+    $db = "saboresdeliz";
+     
+    */ 
 
     $enlace = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME,$DB_PORT);    
+    //$enlace = mysqli_connect($server,$user,$pass,$db);  
     
     if (!$enlace){
         die ("No pudo conectarse a la base de datos");
